13a09e2ce496897f70c596bf35dea76c4b409ae4
marquete <marquete@quov.is>
2014-04-15 16:53:32 -0300

    Convert tabs to spaces in not-pinterested.user.js.

--- not-pinterested.user.js
+++ not-pinterested.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name				Not Pinterested
-// @version			1.0
-// @namespace		https://github.com/marquete
-// @description	Blocks pins containing user-defined keywords.
-// @match				*://www.pinterest.com/*
+// @name        Not Pinterested
+// @version     1.0
+// @namespace   https://github.com/marquete
+// @description Blocks pins containing user-defined keywords.
+// @match       *://www.pinterest.com/*
 // ==/UserScript==
 
 var blacklist, log, monkeyPatch;
@@ -15,20 +15,20 @@ log = false;
 monkeyPatch = document.createElement('script');
 
 monkeyPatch.textContent = 'var filter = new RegExp("' + (log ? '(' : '') + blacklist.join('|') + (log ? ')' : '') + '", "im");\n' +
-													'VariableHeightLayoutManager.prototype._hijackedPositionItem = VariableHeightLayoutManager.prototype._positionItem;\n' +
-													'VariableHeightLayoutManager.prototype._positionItem = function(a) {\n';
+                          'VariableHeightLayoutManager.prototype._hijackedPositionItem = VariableHeightLayoutManager.prototype._positionItem;\n' +
+                          'VariableHeightLayoutManager.prototype._positionItem = function(a) {\n';
 
 if(log) {
-	monkeyPatch.textContent += 	'  var match = filter.exec(a.el.textContent);\n' +
-															'  if(match === null) {\n' +
-															'    this._hijackedPositionItem(a);\n' +
-															'  } else {\n' +
-															'    console.log("not pinterested in " + match[1]);\n' +
-															'  }\n';
+  monkeyPatch.textContent +=  '  var match = filter.exec(a.el.textContent);\n' +
+                              '  if(match === null) {\n' +
+                              '    this._hijackedPositionItem(a);\n' +
+                              '  } else {\n' +
+                              '    console.log("not pinterested in " + match[1]);\n' +
+                              '  }\n';
 } else {
-	monkeyPatch.textContent += 	'  if(!filter.test(a.el.textContent)) {\n' +
-															'    this._hijackedPositionItem(a);\n' +
-															'  }\n';
+  monkeyPatch.textContent +=  '  if(!filter.test(a.el.textContent)) {\n' +
+                              '    this._hijackedPositionItem(a);\n' +
+                              '  }\n';
 }
 
 monkeyPatch.textContent += '};';
